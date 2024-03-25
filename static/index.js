c610db0b9aca2be1dafd804e15d8fc64ab855852
Nitish <1rn19cs092.nitish.pkv@gmail.com>
2024-03-25 14:20:17 +0530

    Final Commit

--- static/index.js
+++ static/index.js
@@ -6,3 +6,14 @@ function deleteNote(noteId) {
     window.location.href = "/home";
   });
 }
+
+function myFunction() {
+  var x = document.lastModified;
+  var lastModified = new Date(x);
+  lastModified.setHours(lastModified.getHours() + 5); // Add 5 hours for IST
+  lastModified.setMinutes(lastModified.getMinutes() + 30); // Add 30 minutes for IST
+  document.getElementById("demo").innerHTML = lastModified.toISOString().substr(11, 8);
+
+
+}
+myFunction()
